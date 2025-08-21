35bd132a228a021381bdd38b681b5c15a2b3a0a3
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-21 16:11:21 +0800

    Submit token obtained from localStorage to backend

--- create-posts/src/components/UserPosts/user-posts.js
+++ create-posts/src/components/UserPosts/user-posts.js
@@ -1,9 +1,19 @@
 export async function getPosts() {
-  // TODO: provide bearer token
-  const response = await fetch("http://localhost:8000/posts/user");
+  const token = localStorage.getItem("token");
+  console.log("token obtained: " + token);
+
+  if (!token) {
+    throw new Error("Token not found.");
+  }
+
+  const response = await fetch("http://localhost:8000/posts/user", {
+    headers: { authorization: `Bearer ${token}` },
+  });
+
   const parsed = await response.json();
   const posts = parsed.posts;
   console.log("posts: " + posts);
+  console.log("returned user: " + JSON.stringify(parsed.user));
 
   return posts;
 }
