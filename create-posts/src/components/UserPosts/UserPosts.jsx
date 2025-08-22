c8af9906772b3e0c56178b36d3f41b13053c9d75
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-22 14:41:24 +0800

    Organize post grid structure

--- create-posts/src/components/UserPosts/UserPosts.jsx
+++ create-posts/src/components/UserPosts/UserPosts.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getPosts } from "./user-posts";
 import { useNavigate } from "react-router-dom";
+import styles from "./UserPosts.module.css";
 
 const UserPosts = () => {
   const [posts, setPosts] = useState([]);
@@ -17,25 +18,30 @@ const UserPosts = () => {
   return (
     <div className="container">
       <h1>Posts</h1>
-      <ul>
+      <ul className={styles.posts}>
         {posts.map((post) => (
           <li key={post.id}>
-            <ul>
-              <li>Title: {post.title}</li>
-              <li>Text: {post.text}</li>
-              <li>
-                Upload time: {new Date(post.addedTime).toLocaleDateString()},{" "}
-                {new Date(post.addedTime).toLocaleTimeString()}
+            <ul className={styles.post}>
+              <li className={styles.postTitle}>{post.title}</li>
+              <li className={styles.postText}>{post.text}</li>
+              <li className={styles.postTime}>
+                <div className={styles.clockIcon}>&#x1F550;:</div>
+                <div>
+                  {post.editedTime
+                    ? `${new Date(post.editedTime).toDateString()}, 
+                    ${new Date(post.editedTime).toLocaleTimeString([], {
+                      hour12: true,
+                    })}`
+                    : `${new Date(post.addedTime).toDateString()}, 
+                    ${new Date(post.addedTime).toLocaleTimeString([], {
+                      hour12: true,
+                    })}`}
+                  {post.editedTime ? <i> (edited)</i> : null}
+                </div>
               </li>
-              {post.editedTime ? (
-                <li>
-                  Edited time: {new Date(post.editedTime).toLocaleDateString()},{" "}
-                  {new Date(post.editedTime).toLocaleTimeString()}
-                </li>
-              ) : null}
-              <li>
+              <li className={styles.postButtons}>
                 <button
-                  className="edit-post"
+                  className={styles.editPost}
                   onClick={() =>
                     navigator("/posts/edit", {
                       state: {
@@ -46,9 +52,7 @@ const UserPosts = () => {
                 >
                   &#x270E;
                 </button>
-              </li>
-              <li>
-                <button className="delete-post">&#x274C;</button>
+                <button className={styles.deletePost}>&#x274C;</button>
               </li>
             </ul>
           </li>
